c878d50d1e998aa57da3bc6173f173dbd47a8410
okuzan <kuzanoleg@gmail.com>
2024-09-15 00:51:27 +0300

    added UI for email verification

--- src/app/email-verification/email-verification.component.ts
+++ src/app/email-verification/email-verification.component.ts
@@ -1,46 +1,92 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {TuiAlertService} from '@taiga-ui/core';
-import {AuthService} from "../auth/auth.service";
+import {AuthService} from '../auth/auth.service';
+import {ParticlesService} from '../services/particles.service';
+import {AsyncPipe, NgClass, NgIf, NgOptimizedImage} from "@angular/common";
+import {NgxParticlesModule} from "@tsparticles/angular";
 
 @Component({
   selector: 'app-email-verification',
-  template: `
-    <div class="verification-container">
-      <h1>Email Verification</h1>
-      <p>{{ message }}</p>
-    </div>`,
+  templateUrl: './email-verification.component.html',
+  styleUrls: ['./email-verification.component.less'],
   standalone: true,
+  imports: [
+    NgxParticlesModule,
+    AsyncPipe,
+    NgIf,
+    NgClass,
+    NgOptimizedImage
+  ],
 })
 export class EmailVerificationComponent implements OnInit {
+  private readonly REDIRECT_TIMEOUT = 3000;
+  private readonly TOKEN_VERIFICATION_TIMEOUT = 3000;
+
   message: string = 'Verifying...';
+  verificationCompleted: boolean = false;
+  verificationSuccess: boolean = false;
+  isRotating: boolean = true;
+
+  id = 'tsparticles';
+  particlesOptions$ = this.particlesService.particlesOptions$;
 
   constructor(
     private authService: AuthService,
     private route: ActivatedRoute,
-    private router: Router,
-    private alertService: TuiAlertService
+    public router: Router,
+    private alertService: TuiAlertService,
+    public particlesService: ParticlesService
   ) {
   }
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      const token = params['token'];
-      if (token) {
-        this.authService.verifyEmail(token).subscribe({
-          next: () => {
-            this.message = 'Email verified successfully!';
-            this.alertService.open(this.message, {status: 'success'}).subscribe();
-            setTimeout(() => this.router.navigate(['/auth']), 3000);
-          },
-          error: error => {
-            this.message = error.error.message || 'Email verification failed';
-            this.alertService.open(this.message, {status: 'error'}).subscribe();
-          }
-        });
-      } else {
-        this.message = 'No token provided';
-      }
-    });
+    // Initialize particles
+    this.particlesService.initParticles();
+    this.particlesService.loadParticlesOptions();
+
+    // Start the verification process
+    setTimeout(() => this.route.queryParams.subscribe({
+      next: (params) => {
+        const token = params['token'];
+        if (token) {
+          this.authService.verifyEmail(token).subscribe({
+            next: () => {
+              this.verificationSuccess = true;
+              this.message = 'Email verified successfully!';
+              this.stopRotation();
+              this.displayResult('success');
+            },
+            error: (error) => {
+              this.verificationSuccess = false;
+              this.message = error.error.message || 'Email verification failed';
+              this.stopRotation();
+              this.displayResult('error');
+            },
+          });
+        } else {
+          this.verificationSuccess = false;
+          this.message = 'No token provided';
+          this.stopRotation();
+          this.displayResult('error');
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching query params:', error);
+        this.stopRotation();
+      },
+    }), this.TOKEN_VERIFICATION_TIMEOUT);
+  }
+
+  private stopRotation() {
+    this.isRotating = false;
+  }
+
+  private displayResult(result: 'success' | 'error') {
+    this.verificationCompleted = true;
+    this.alertService.open(this.message, {status: result}).subscribe();
+    if (result === 'success') {
+      setTimeout(() => this.router.navigate(['/auth'], {fragment: 'sign-in'}), this.REDIRECT_TIMEOUT);
+    }
   }
 }
