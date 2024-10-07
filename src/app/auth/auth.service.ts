22d6b08bf86021f5e1c2cdafc9478c09fc99d5b9
okuzan <kuzanoleg@gmail.com>
2024-10-07 23:08:16 +0300

    httponly

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -48,8 +48,8 @@ export class AuthService {
 
   clearCookies(): void {
     // Clear any stored tokens or session information
-    document.cookie = 'accessToken=; Max-Age=0; path=/; domain=.api.almonium.com; Secure';
-    document.cookie = 'refreshToken=; Max-Age=0; path=/api/v1/public/auth/refresh-token; domain=.api.almonium.com; Secure';
+    document.cookie = 'accessToken=; Max-Age=0; path=/; domain=.api.almonium.com; Secure; HttpOnly';
+    document.cookie = 'refreshToken=; Max-Age=0; path=/api/v1/public/auth/refresh-token; domain=.api.almonium.com; Secure; HttpOnly';
     console.log('Cookies cleared');
   }
 
