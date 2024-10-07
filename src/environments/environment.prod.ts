4af87d3e6fbe68eb9ee115f4c5475d2551ac4dcd
okuzan <kuzanoleg@gmail.com>
2024-10-07 18:50:11 +0300

    added property to prod env ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.almonium.com',
-  feUrl: 'https://almonium.com'
+  feUrl: 'https://almonium.com',
+  googleClientId: '33380019461-l4hq3eto00vvvgprtadahm5o2tqj65ti.apps.googleusercontent.com',
 };
