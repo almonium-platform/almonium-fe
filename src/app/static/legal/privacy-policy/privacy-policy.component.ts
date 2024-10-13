22e722f4e276b4ca06c273917f1d6fe47438d097
okuzan <kuzanoleg@gmail.com>
2024-10-13 21:42:43 +0300

    privacy policy and terms of use not ready

--- src/app/static/legal/privacy-policy/privacy-policy.component.ts
+++ src/app/static/legal/privacy-policy/privacy-policy.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
+import {NavbarComponent} from "../../../shared/navbar/navbar.component";
+import {NotReadyComponent} from "../../../shared/not-ready/not-ready.component";
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    NotReadyComponent
+  ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.less'
 })
--- src/app/static/legal/terms-of-use/terms-of-use.component.ts
+++ src/app/static/legal/terms-of-use/terms-of-use.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
+import {NavbarComponent} from "../../../shared/navbar/navbar.component";
+import {NotReadyComponent} from "../../../shared/not-ready/not-ready.component";
 
 @Component({
   selector: 'app-terms-of-use',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    NotReadyComponent
+  ],
   templateUrl: './terms-of-use.component.html',
   styleUrl: './terms-of-use.component.less'
 })
