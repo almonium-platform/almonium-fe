f0aec5c92d266a9cd4d3dba74d01efcd2ae7a21d
okuzan <kuzanoleg@gmail.com>
2024-10-19 23:02:15 +0300

    Revert "added sentry"
    This reverts commit f693777f39701f735e5ce22ced50c02822ac5ad5.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,14 @@
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {TuiRootModule} from "@taiga-ui/core";
-import {ApplicationConfig, ErrorHandler, importProvidersFrom} from '@angular/core';
-import {provideRouter, Router} from '@angular/router';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {TokenInterceptor} from "./authentication/auth/token-interceptor";
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
-import * as Sentry from "@sentry/angular";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    {
-      provide: ErrorHandler,
-      useValue: Sentry.createErrorHandler(),
-    },
-    {
-      provide: Sentry.TraceService,
-      deps: [Router],
-    },
     provideAnimations(),
     provideRouter(routes),
     importProvidersFrom(TuiRootModule),
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,5 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
 import {appConfig} from "./app/app.config";
-import * as Sentry from "@sentry/angular";
 
-Sentry.init({
-  dsn: "https://92feb48a60b8eb5d5d2c9f033f34ad9a@o4508143201288192.ingest.de.sentry.io/4508143229993040",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
