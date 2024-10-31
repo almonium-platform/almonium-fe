185cfe8c4e73fafa0baaf6ddeef8a34530ccaf32
okuzan <kuzanoleg@gmail.com>
2024-10-31 23:38:56 +0200

    Confirm Modal - responsive

--- src/app/shared/modals/confirm-modal/confirm-modal.component.ts
+++ src/app/shared/modals/confirm-modal/confirm-modal.component.ts
@@ -12,21 +12,18 @@ import {DismissButtonComponent} from "../elements/dismiss-button/dismiss-button.
   ],
   template: `
     <div class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" *ngIf="isVisible">
-      <div class="bg-white rounded-3xl w-96 p-7 relative">
+      <div class="bg-white rounded-3xl w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg p-7 relative">
         <app-dismiss-button (close)="onClose()"></app-dismiss-button>
         <div class="flex items-center mb-4 flex-row">
           <img ngSrc="../../../../assets/img/icons/alert-triangle.svg" alt="Warning Icon" class="icon" height="512"
                width="512" style="width: 20px; height: 20px; margin-right: 6px; margin-top: 2px">
-          <!-- Use dynamic title here -->
           <h2 class="text-xl font-bold ml-0.5">{{ title }}</h2>
         </div>
-        <!-- Use dynamic message here -->
         <p class="text-gray-700 mb-6 mt-6 text-sm">{{ message }}</p>
         <div class="flex justify-between">
           <button (click)="onClose()" class="text-gray-950 underline font-bold hover:underline ">Cancel</button>
           <button (click)="onConfirm()" [disabled]="isButtonDisabled"
                   class="bg-red-500 text-white px-4 py-2 font-bold rounded-3xl hover:bg-red-600 disabled:bg-gray-400 disabled:cursor-not-allowed">
-            <!-- Use dynamic confirmText here -->
             {{ isButtonDisabled ? 'Proceed in ' + countdown : confirmText }}
           </button>
         </div>
