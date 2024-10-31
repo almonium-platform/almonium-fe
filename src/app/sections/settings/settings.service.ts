1b996693de313de82430f30a2da63303fec5338f
okuzan <kuzanoleg@gmail.com>
2024-10-31 23:40:01 +0200

    Unlink checks if we need to reauth. New auth mgmt endpoints

--- src/app/sections/settings/settings.service.ts
+++ src/app/sections/settings/settings.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AppConstants} from "../../app.constants";
 import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
 
 @Injectable({
@@ -21,9 +22,12 @@ export class SettingService {
     return this.http.get<boolean>(url, {withCredentials: true});
   }
 
-  unlinkAuthProvider(provider: string): Observable<any> {
-    const url = `${AppConstants.AUTH_URL}/providers/${provider}`;
-    return this.http.delete(url, {withCredentials: true});
+  unlinkAuthProvider(provider: string): Observable<boolean> {
+    const url = `${AppConstants.AUTH_URL}/providers/${provider.toUpperCase()}`;
+    return this.http.delete<{ reauthRequired: boolean }>(url, {withCredentials: true})
+      .pipe(
+        map(response => response.reauthRequired)
+      );
   }
 
   getAuthProviders(): Observable<string[]> {
@@ -34,5 +38,25 @@ export class SettingService {
   deleteAccount(): Observable<any> {
     const url = `${AppConstants.USERS_URL}/me`;
     return this.http.delete(url, {withCredentials: true});
+  }
+
+  isEmailAvailable(email: string): Observable<boolean> {
+    const url = `${AppConstants.AUTH_URL}/email-availability`;
+    return this.http.post<boolean>(url, {email}, {withCredentials: true});
+  }
+
+  requestEmailChange(email: string): Observable<any> {
+    const url = `${AppConstants.AUTH_URL}/email-changes/request`;
+    return this.http.post<any>(url, {email}, {withCredentials: true});
+  }
+
+  changePassword(newPassword: string): Observable<void> {
+    const url = `${AppConstants.AUTH_URL}/password`;
+    return this.http.put<void>(url, {password: newPassword}, {withCredentials: true});
+  }
+
+  requestEmailVerification(): Observable<void> {
+    const url = `${AppConstants.AUTH_URL}/email-verification/request`;
+    return this.http.post<void>(url, {}, { withCredentials: true });
   }
 }
