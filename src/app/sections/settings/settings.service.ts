b09f24a8cee53bbdede5e2505bac02410f1fd32e
okuzan <kuzanoleg@gmail.com>
2024-11-02 23:47:23 +0200

    BE endpoints, settings service

--- src/app/sections/settings/settings.service.ts
+++ src/app/sections/settings/settings.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {AppConstants} from "../../app.constants";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
+import {AuthProvider, TokenInfo} from "./auth.types";
 
 
 @Injectable({
@@ -12,14 +13,52 @@ export class SettingService {
   constructor(private http: HttpClient) {
   }
 
-  isEmailVerified(): Observable<boolean> {
-    const url = `${AppConstants.AUTH_URL}/email-verified`;
-    return this.http.get<boolean>(url, {withCredentials: true});
-  }
-
   checkCurrentAccessToken(): Observable<boolean> {
     const url = `${AppConstants.AUTH_URL}/access-token/verify-live`;
     return this.http.get<boolean>(url, {withCredentials: true});
+  }
+
+  // email change request
+  requestEmailChange(email: string): Observable<any> {
+    const url = `${AppConstants.EMAIL_CHANGE_REQUEST_URL}`;
+    return this.http.post<any>(url, {email}, {withCredentials: true});
+  }
+
+  cancelEmailChangeRequest(): Observable<any> {
+    const url = `${AppConstants.EMAIL_CHANGE_REQUEST_URL}`;
+    return this.http.delete<any>(url, {withCredentials: true});
+  }
+
+  resendEmailChangeRequest(): Observable<void> {
+    const url = `${AppConstants.EMAIL_CHANGE_REQUEST_URL}/resend`;
+    return this.http.post<void>(url, {}, {withCredentials: true});
+  }
+
+  // email verification requests
+  isEmailVerified(): Observable<boolean> {
+    const url = `${AppConstants.EMAIL_VERIFICATION_URL}/me`;
+    return this.http.get<boolean>(url, {withCredentials: true});
+  }
+
+  requestEmailVerification(): Observable<void> {
+    const url = `${AppConstants.EMAIL_VERIFICATION_URL}/request`;
+    return this.http.post<void>(url, {}, {withCredentials: true});
+  }
+
+  getLastEmailVerificationToken(): Observable<TokenInfo> {
+    const url = `${AppConstants.EMAIL_VERIFICATION_URL}/last-token`;
+    return this.http.get<TokenInfo>(url, {withCredentials: true});
+  }
+
+  // sensitive
+  changePassword(newPassword: string): Observable<void> {
+    const url = `${AppConstants.AUTH_URL}/password`;
+    return this.http.put<void>(url, {password: newPassword}, {withCredentials: true});
+  }
+
+  deleteAccount(): Observable<any> {
+    const url = `${AppConstants.AUTH_URL}/me`;
+    return this.http.delete(url, {withCredentials: true});
   }
 
   unlinkAuthProvider(provider: string): Observable<boolean> {
@@ -30,33 +69,14 @@ export class SettingService {
       );
   }
 
-  getAuthProviders(): Observable<string[]> {
+  // auth data retrieval
+  getAuthMethods(): Observable<AuthProvider[]> {
     const url = `${AppConstants.AUTH_URL}/providers`;
-    return this.http.get<string[]>(url, {withCredentials: true});
-  }
-
-  deleteAccount(): Observable<any> {
-    const url = `${AppConstants.USERS_URL}/me`;
-    return this.http.delete(url, {withCredentials: true});
+    return this.http.get<AuthProvider[]>(url, {withCredentials: true});
   }
 
   isEmailAvailable(email: string): Observable<boolean> {
-    const url = `${AppConstants.AUTH_URL}/email-availability`;
+    const url = `${AppConstants.AUTH_URL}/email/availability`;
     return this.http.post<boolean>(url, {email}, {withCredentials: true});
-  }
-
-  requestEmailChange(email: string): Observable<any> {
-    const url = `${AppConstants.EMAIL_CHANGE_REQUEST_URL}`;
-    return this.http.post<any>(url, {email}, {withCredentials: true});
-  }
-
-  changePassword(newPassword: string): Observable<void> {
-    const url = `${AppConstants.AUTH_URL}/password`;
-    return this.http.put<void>(url, {password: newPassword}, {withCredentials: true});
-  }
-
-  requestEmailVerification(): Observable<void> {
-    const url = `${AppConstants.AUTH_URL}/email-verification/request`;
-    return this.http.post<void>(url, {}, {withCredentials: true});
   }
 }
