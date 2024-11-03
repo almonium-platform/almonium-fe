c4319b97331edff22e6c663ad00dc36cd96206ad
okuzan <kuzanoleg@gmail.com>
2024-11-03 18:42:33 +0200

    responsive action modal - hide close button for small screens

--- src/app/shared/modals/action-modal/action-modal.component.ts
+++ src/app/shared/modals/action-modal/action-modal.component.ts
@@ -24,7 +24,7 @@ import {DismissButtonComponent} from "../elements/dismiss-button/dismiss-button.
         </div>
         <p class="text-gray-700 mb-6 mt-6 text-sm" [innerHTML]="message"></p>
         <div class="flex justify-between">
-          <button (click)="onClose()" class="text-gray-950 underline font-bold hover:underline ">Close</button>
+          <button (click)="onClose()" class="hidden sm:block text-gray-950 underline font-bold hover:underline">Close</button>
           <button (click)="onConfirmTwo()"
                   class="bg-white border border-black text-black px-4 py-2 font-bold rounded-3xl hover:bg-gray-100">
             {{ actionTwoText }}
