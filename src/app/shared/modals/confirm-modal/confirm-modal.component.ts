64046a7504655ac87f83d93fe30e1588a3f0bdd3
okuzan <kuzanoleg@gmail.com>
2024-11-24 21:34:24 +0200

    confirm modal - fade in/out animations, darker BG

--- src/app/shared/modals/confirm-modal/confirm-modal.component.ts
+++ src/app/shared/modals/confirm-modal/confirm-modal.component.ts
@@ -9,23 +9,42 @@ import {DismissButtonComponent} from "../elements/dismiss-button/dismiss-button.
     NgIf,
     DismissButtonComponent
   ],
+  styleUrls: ['./confirm-modal.component.less'],
   template: `
-    <div class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" *ngIf="isVisible">
-      <div class="bg-white rounded-3xl w-full max-w-xs sm:max-w-sm p-7 relative">
-        <app-dismiss-button (close)="onClose()"></app-dismiss-button>
+    <div
+      class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
+      *ngIf="isVisible"
+    >
+      <div
+        [class.fade-slide-in]="isVisible"
+        [class.fade-slide-out]="fadeOutAnimating"
+        class="bg-white rounded-3xl w-full max-w-xs sm:max-w-sm p-7 relative"
+      >
+        <app-dismiss-button (close)="onClose()"
+        ></app-dismiss-button>
         <div class="flex items-center mb-4 flex-row">
-          <span class="flex items-center justify-center" style="margin-right: 6px">
-            <i class="fas fa-circle-exclamation text-xl text-red-500"
-               style="margin-top: 1px"
-            ></i>
-            </span>
+      <span
+        class="flex items-center justify-center" style="margin-right: 6px">
+        <i
+          class="fas fa-circle-exclamation text-xl text-red-500"
+          style="margin-top: 1px"
+        ></i>
+      </span>
           <h2 class="text-xl font-bold ml-0.5">{{ title }}</h2>
         </div>
         <p class="text-gray-700 mb-6 mt-6 text-sm">{{ message }}</p>
         <div class="flex justify-between">
-          <button (click)="onClose()" class="text-gray-950 underline font-bold hover:underline ">Cancel</button>
-          <button (click)="onConfirm()" [disabled]="isButtonDisabled"
-                  class="bg-red-500 text-white px-4 py-2 font-bold rounded-3xl hover:bg-red-600 disabled:bg-gray-400 disabled:cursor-not-allowed">
+          <button
+            (click)="onClose()"
+            class="text-gray-950 underline font-bold hover:underline"
+          >
+            Cancel
+          </button>
+          <button
+            (click)="onConfirm()"
+            [disabled]="isButtonDisabled"
+            class="bg-red-500 text-white px-4 py-2 font-bold rounded-3xl hover:bg-red-600 disabled:bg-gray-400 disabled:cursor-not-allowed"
+          >
             {{ isButtonDisabled ? 'Proceed in ' + countdown : confirmText }}
           </button>
         </div>
@@ -43,6 +62,7 @@ export class ConfirmModalComponent implements OnChanges, OnDestroy {
   @Output() close = new EventEmitter<void>();
   @Output() confirm = new EventEmitter<void>();
 
+  fadeOutAnimating: boolean = false;
   countdown: number = 5;
   isButtonDisabled: boolean = true;
   intervalId: any;
@@ -82,7 +102,11 @@ export class ConfirmModalComponent implements OnChanges, OnDestroy {
   }
 
   onClose() {
-    this.close.emit();
+    this.fadeOutAnimating = true;
+    setTimeout(() => {
+      this.close.emit();
+      this.fadeOutAnimating = false;
+    }, 300); // Match animation duration in milliseconds
   }
 
   onConfirm() {
