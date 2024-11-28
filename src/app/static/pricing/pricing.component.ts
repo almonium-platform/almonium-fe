7432914cbb4c7c6c84d6d4b41350e66521f6b97c
okuzan <kuzanoleg@gmail.com>
2024-11-28 23:34:48 +0200

    pricing component, uses paywall ng template

--- src/app/static/pricing/pricing.component.ts
+++ src/app/static/pricing/pricing.component.ts
@@ -1,16 +1,30 @@
-import {Component} from '@angular/core';
-import {NotReadyComponent} from "../../shared/not-ready/not-ready.component";
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {NavbarWrapperComponent} from "../../shared/navbars/navbar-wrapper/navbar-wrapper.component";
+import {PlanService} from "../../services/plan.service";
+import {PaywallComponent} from "../../shared/paywall/paywall.component";
+import {NgIf, NgTemplateOutlet} from "@angular/common";
 
 @Component({
-    selector: 'app-pricing',
-    imports: [
-        NotReadyComponent,
-        NavbarWrapperComponent
-    ],
-    templateUrl: './pricing.component.html',
-    styleUrl: './pricing.component.less'
+  selector: 'app-pricing',
+  imports: [
+    NavbarWrapperComponent,
+    NgTemplateOutlet,
+    NgIf,
+    PaywallComponent
+  ],
+  templateUrl: './pricing.component.html',
+  styleUrl: './pricing.component.less'
 })
-export class PricingComponent {
+export class PricingComponent implements OnInit {
+  @ViewChild(PaywallComponent, {static: true}) paywallComponent!: PaywallComponent;
+
+  constructor(private planService: PlanService,) {
+  }
+
+  ngOnInit(): void {
+    this.planService.getPlans().subscribe(plans => {
+      console.log(plans);
+    });
+  }
 
 }
