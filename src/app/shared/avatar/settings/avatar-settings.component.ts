ceeed6fb32c47004cec74ddc6aaab6edfd04842d
okuzan <kuzanoleg@gmail.com>
2025-01-01 23:43:20 +0200

    extracted avatar settings from profile settings to reusable components.

---
+++ src/app/shared/avatar/settings/avatar-settings.component.ts
@@ -0,0 +1,50 @@
+import {Component, Input, ViewChild} from '@angular/core';
+import {ManageAvatarComponent} from "../../../sections/settings/profile/avatar/manage-avatar/manage-avatar.component";
+import {PopupTemplateStateService} from "../../modals/popup-template/popup-template-state.service";
+import {ProfilePictureComponent} from "../profile-picture.component";
+import {TuiIcon} from "@taiga-ui/core";
+import {TuiBadge, TuiBadgedContent} from "@taiga-ui/kit";
+
+@Component({
+  selector: 'app-avatar-settings',
+  templateUrl: './avatar-settings.component.html',
+  styleUrls: ['./avatar-settings.component.less'],
+  imports: [
+    ProfilePictureComponent,
+    TuiIcon,
+    TuiBadgedContent,
+    TuiBadge,
+    ManageAvatarComponent
+  ]
+})
+export class AvatarSettingsComponent {
+  @Input() userInfo!: {
+    avatarUrl: string | null;
+    username: string | null;
+    premium: boolean;
+  };
+
+  @Input() size: 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl' = 'xxl';
+  @Input() sizeInRem: number | null = null;
+  @ViewChild(ManageAvatarComponent, {static: true}) manageAvatarComponent!: ManageAvatarComponent;
+
+  constructor(
+    private popupTemplateStateService: PopupTemplateStateService) {
+  }
+
+  changeAvatar(): void {
+    this.popupTemplateStateService.open(this.manageAvatarComponent.content);
+  }
+
+  get avatarUrl(): string | null {
+    return this.userInfo?.avatarUrl || null;
+  }
+
+  get username(): string | null {
+    return this.userInfo?.username || null;
+  }
+
+  get outline(): boolean {
+    return this.userInfo?.premium || false;
+  }
+}
