dedd11ff94e936983f610b16e3a2705ef8f729a2
okuzan <kuzanoleg@gmail.com>
2025-01-08 23:22:04 +0200

    destroy recent-auth guard on close

--- src/app/shared/recent-auth-guard/recent-auth-guard-state.service.ts
+++ src/app/shared/recent-auth-guard/recent-auth-guard-state.service.ts
@@ -1,11 +1,11 @@
 import {Injectable} from '@angular/core';
-import {Subject} from 'rxjs';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecentAuthGuardStateService {
-  private recentAuthState = new Subject<{ visible: boolean }>();
+  private recentAuthState = new BehaviorSubject<{ visible: boolean }>({visible: false});
   recentAuthState$ = this.recentAuthState.asObservable();
 
   open() {
--- src/app/shared/recent-auth-guard/recent-auth-guard.component.ts
+++ src/app/shared/recent-auth-guard/recent-auth-guard.component.ts
@@ -1,25 +1,34 @@
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {RecentAuthGuardStateService} from "./recent-auth-guard-state.service";
 import {AuthComponent} from "../../authentication/auth/auth.component";
-import {Subject, takeUntil} from "rxjs";
+import {filter, Subject, takeUntil} from "rxjs";
 import {PopupTemplateStateService} from "../modals/popup-template/popup-template-state.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-recent-auth-guard',
   template: `
-    <app-auth mode="embedded"></app-auth>
+    <ng-container *ngIf="isAuthModalVisible">
+      <app-auth
+        mode="embedded"
+      ></app-auth>
+    </ng-container>
   `,
   imports: [
-    AuthComponent
+    AuthComponent,
+    NgIf
   ],
 })
 export class RecentAuthGuardComponent implements OnInit, OnDestroy {
   private readonly destroy$ = new Subject<void>();
-  @ViewChild(AuthComponent, {static: true}) authComponent!: AuthComponent;
+  @ViewChild(AuthComponent, {static: false}) authComponent!: AuthComponent;
+
+  protected isAuthModalVisible = false;
 
   constructor(
     private recentGuardService: RecentAuthGuardStateService,
     private popupTemplateStateService: PopupTemplateStateService,
+    private cdr: ChangeDetectorRef,
   ) {
   }
 
@@ -27,13 +36,29 @@ export class RecentAuthGuardComponent implements OnInit, OnDestroy {
     this.recentGuardService.recentAuthState$
       .pipe(takeUntil(this.destroy$))
       .subscribe((state) => {
+          this.isAuthModalVisible = state.visible;
           if (state.visible) {
-            this.popupTemplateStateService.open(this.authComponent.content, 'auth', true);
-          } else {
-            this.popupTemplateStateService.close();
+            setTimeout(() => {
+              if (this.authComponent && this.authComponent.content) {
+                this.popupTemplateStateService.open(this.authComponent.content, 'auth', true);
+              } else {
+                console.error('AuthComponent or its content is undefined.');
+              }
+            });
           }
         }
       );
+
+    this.popupTemplateStateService.drawerState$
+      .pipe(
+        takeUntil(this.destroy$),
+        filter((state) => state.type === 'auth' && !state.visible)
+      ).subscribe((state) => {
+      if (!state.visible && state.type === 'auth') {
+        this.isAuthModalVisible = false;
+        this.cdr.detectChanges();
+      }
+    });
   }
 
   ngOnDestroy() {
