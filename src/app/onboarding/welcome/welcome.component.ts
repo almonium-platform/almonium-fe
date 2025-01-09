c713cdcf790a7d553a22b8a5c3dc710f45d39113
okuzan <kuzanoleg@gmail.com>
2025-01-09 18:35:42 +0200

    welcome - use app-button

--- src/app/onboarding/welcome/welcome.component.ts
+++ src/app/onboarding/welcome/welcome.component.ts
@@ -3,13 +3,15 @@ import {TuiAlertService} from "@taiga-ui/core";
 import {UserInfoService} from "../../services/user-info.service";
 import {isStepAfter, SetupStep, UserInfo} from "../../models/userinfo.model";
 import {OnboardingService} from "../onboarding.service";
-import {Subject, takeUntil} from "rxjs";
+import {BehaviorSubject, finalize, Subject, takeUntil} from "rxjs";
 import {GifPlayerComponent} from "../../shared/gif-player/gif-player.component";
+import {ButtonComponent} from "../../shared/button/button.component";
 
 @Component({
   selector: 'app-welcome',
   imports: [
-    GifPlayerComponent
+    GifPlayerComponent,
+    ButtonComponent
   ],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.less'
@@ -23,6 +25,9 @@ export class WelcomeComponent implements OnInit, OnDestroy {
 
   protected userInfo: UserInfo | null = null;
   protected replayGifSubject = new Subject<void>();
+
+  private readonly loadingSubject$ = new BehaviorSubject<boolean>(false);
+  protected readonly loading$ = this.loadingSubject$.asObservable();
 
   constructor(
     private onboardingService: OnboardingService,
@@ -55,14 +60,17 @@ export class WelcomeComponent implements OnInit, OnDestroy {
       return;
     }
 
-    this.onboardingService.completeStep(this.step).subscribe({
-      next: () => {
-        this.userInfoService.updateUserInfo({setupStep: nextStep});
-      },
-      error: (error) => {
-        console.error('Failed to start onboarding', error);
-        this.alertService.open('Failed to start onboarding', {appearance: 'error'}).subscribe()
-      }
-    });
+    this.loadingSubject$.next(true);
+    this.onboardingService.completeStep(this.step)
+      .pipe(finalize(() => this.loadingSubject$.next(false)))
+      .subscribe({
+        next: () => {
+          this.userInfoService.updateUserInfo({setupStep: nextStep});
+        },
+        error: (error) => {
+          console.error('Failed to start onboarding', error);
+          this.alertService.open('Failed to start onboarding', {appearance: 'error'}).subscribe()
+        },
+      });
   }
 }
