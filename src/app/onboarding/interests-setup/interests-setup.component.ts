07b5d9c5bc0d81966fe2ee63f8fee80ced7837f5
okuzan <kuzanoleg@gmail.com>
2025-01-09 20:25:39 +0200

    interests setup button

--- src/app/onboarding/interests-setup/interests-setup.component.ts
+++ src/app/onboarding/interests-setup/interests-setup.component.ts
@@ -6,12 +6,15 @@ import {SetupStep} from "../../models/userinfo.model";
 import {Interest} from "../../shared/interests/interest.model";
 import {FormsModule} from "@angular/forms";
 import {InterestsComponent} from "../../shared/interests/interests.component";
+import {ButtonComponent} from "../../shared/button/button.component";
+import {BehaviorSubject, finalize} from "rxjs";
 
 @Component({
   selector: 'app-interests-setup',
   imports: [
     FormsModule,
-    InterestsComponent
+    InterestsComponent,
+    ButtonComponent
   ],
   templateUrl: './interests-setup.component.html',
   styleUrl: './interests-setup.component.less'
@@ -19,6 +22,9 @@ import {InterestsComponent} from "../../shared/interests/interests.component";
 export class InterestsSetupComponent {
   @Input() currentInterests: Interest[] = [];
   protected selectedInterests: Interest[] = [];
+
+  private readonly loadingSubject$ = new BehaviorSubject<boolean>(false);
+  protected readonly loading$ = this.loadingSubject$.asObservable();
 
   constructor(
     private onboardingService: OnboardingService,
@@ -28,15 +34,19 @@ export class InterestsSetupComponent {
   }
 
   submit() {
-    this.onboardingService.saveInterests(this.selectedInterests.map(i => i.id)).subscribe({
-      next: () => {
-        this.userInfoService.updateUserInfo({setupStep: SetupStep.COMPLETED});
-      },
-      error: (error) => {
-        console.error('Failed to save interests', error);
-        this.alertService.open('Failed to save interests', {appearance: 'error'}).subscribe();
-      }
-    });
+    this.loadingSubject$.next(true);
+
+    this.onboardingService.saveInterests(this.selectedInterests.map(i => i.id))
+      .pipe(finalize(() => this.loadingSubject$.next(false)))
+      .subscribe({
+        next: () => {
+          this.userInfoService.updateUserInfo({setupStep: SetupStep.COMPLETED});
+        },
+        error: (error) => {
+          console.error('Failed to save interests', error);
+          this.alertService.open('Failed to save interests', {appearance: 'error'}).subscribe();
+        }
+      });
   }
 
   onSelectedInterestsChange(interests: Interest[]) {
