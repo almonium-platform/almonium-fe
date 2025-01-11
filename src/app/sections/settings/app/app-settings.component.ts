f150af421348cbec398b1b118f8a4584aeb40e72
okuzan <kuzanoleg@gmail.com>
2025-01-11 20:48:39 +0200

    app settings base

---
+++ src/app/sections/settings/app/app-settings.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {TuiInputModule, TuiInputNumberModule, TuiTextfieldControllerModule} from "@taiga-ui/legacy";
+import {SettingsTabsComponent} from "../tabs/settings-tabs.component";
+
+@Component({
+  selector: 'app-app-settings',
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiInputNumberModule,
+    TuiTextfieldControllerModule,
+    SettingsTabsComponent
+  ],
+  templateUrl: './app-settings.component.html',
+  styleUrl: './app-settings.component.less'
+})
+export class AppSettingsComponent implements OnInit, OnDestroy {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
+}
--- src/app/sections/settings/settings-routing.model.ts
+++ src/app/sections/settings/settings-routing.model.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {AuthSettingsComponent} from './auth/auth-settings.component';
 import {LangSettingsComponent} from "./lang/lang-settings.component";
 import {ProfileSettingsComponent} from "./profile/profile-settings.component";
+import {AppSettingsComponent} from "./app/app-settings.component";
 
 const settingsRoutes: Routes = [
   {path: '', redirectTo: 'me', pathMatch: 'full'},
   {path: 'auth', component: AuthSettingsComponent},
   {path: 'lang', component: LangSettingsComponent},
   {path: 'me', component: ProfileSettingsComponent},
+  {path: 'app', component: AppSettingsComponent},
 ];
 
 @NgModule({
