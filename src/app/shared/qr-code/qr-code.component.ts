5b7774ce1e21b7a80ae5425568dd526dafff7c01
okuzan <kuzanoleg@gmail.com>
2025-01-19 16:18:16 +0200

    QR code component style update, use skeleton

--- src/app/shared/qr-code/qr-code.component.ts
+++ src/app/shared/qr-code/qr-code.component.ts
@@ -1,20 +1,29 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {UtilsService} from "../../services/utils.service";
+import {UtilsService} from '../../services/utils.service';
+import {TuiSkeleton} from "@taiga-ui/kit";
 import {NgStyle} from "@angular/common";
+import {TuiAlertService} from "@taiga-ui/core";
 
 @Component({
   selector: 'app-qr-code',
   templateUrl: './qr-code.component.html',
+  styleUrls: ['./qr-code.component.less'],
   imports: [
+    TuiSkeleton,
     NgStyle
-  ],
-  styleUrls: ['./qr-code.component.less']
+  ]
 })
 export class QRCodeComponent implements OnInit {
-  @Input() textToEncode: string = 'https://almonium.com/users/1234';
+  @Input() textToEncode: string = 'https://almonium.com';
   qrCodeUrl: string | undefined;
+  skeletonImgUrl: string = 'assets/img/other/qr-skeleton.png';
 
-  constructor(private utilsService: UtilsService) {
+  constructor(private utilsService: UtilsService,
+              private alertService: TuiAlertService) {
+  }
+
+  get qrCodeImgUrl(): string | undefined {
+    return this.qrCodeUrl || undefined;
   }
 
   ngOnInit(): void {
@@ -27,9 +36,11 @@ export class QRCodeComponent implements OnInit {
     this.utilsService.getQrCodeUrl(this.textToEncode).subscribe({
       next: (url: string) => {
         this.qrCodeUrl = url;
-        console.log('QR Code URL:', this.qrCodeUrl); // Debug
       },
-      error: (err) => console.error('Failed to generate QR code:', err)
+      error: (err) => {
+        console.error('Failed to generate QR code:', err);
+        this.alertService.open('Failed to generate QR code', {appearance: 'error'}).subscribe();
+      },
     });
   }
 }
