1741e624151b0c6767160829abce875bb5b24b44
okuzan <kuzanoleg@gmail.com>
2025-03-15 22:30:12 +0200

    timer component styling

--- src/app/shared/navbars/navbar/timer/timer.component.ts
+++ src/app/shared/navbars/navbar/timer/timer.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ButtonComponent} from "../../../button/button.component";
+import {LucideAngularModule} from "lucide-angular";
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
+  imports: [
+    ButtonComponent,
+    LucideAngularModule
+  ],
   styleUrls: ['./timer.component.less']
 })
 export class TimerComponent {
-  isTimerOpen = true; // Controls popover visibility
-  firstDigit = 0;
-  secondDigit = 0;
+  private readonly DEFAULT_FIRST_DIGIT = 1;
+  private readonly DEFAULT_SECOND_DIGIT = 0;
+
+  protected firstDigit = this.DEFAULT_FIRST_DIGIT;
+  protected secondDigit = this.DEFAULT_SECOND_DIGIT
+
+  protected state: 'ready' | 'going' | 'paused' = 'ready';
 
   // Increases the digit but keeps it within 0-9
   increaseDigit(position: 'first' | 'second') {
@@ -20,7 +30,6 @@ export class TimerComponent {
     }
   }
 
-  // Decreases the digit but keeps it within 0-9
   decreaseDigit(position: 'first' | 'second') {
     if (position === 'first' && this.firstDigit > 0) {
       this.firstDigit--;
@@ -31,11 +40,30 @@ export class TimerComponent {
   }
 
   startTimer() {
-    const totalTime = this.firstDigit * 10 + this.secondDigit;
-    console.log(`Timer started for ${totalTime} seconds`);
+    this.state = 'going';
   }
 
-  timerOnClickOutside(event: Event) {
-    this.isTimerOpen = false; // Close when clicking outside
+  protected getTotalTime() {
+    return this.firstDigit * 10 + this.secondDigit;
+  }
+
+  stopTimer() {
+    this.firstDigit = this.DEFAULT_FIRST_DIGIT;
+    this.secondDigit = this.DEFAULT_SECOND_DIGIT
+    this.state = 'ready';
+  }
+
+  showArrows() {
+    return this.state !== 'ready';
+  }
+
+  togglePauseTimer() {
+    if (this.state === 'paused') {
+      this.state = 'going';
+    } else if (this.state === 'going') {
+      this.state = 'paused';
+    } else {
+      console.error('Invalid state');
+    }
   }
 }
