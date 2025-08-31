1944fbb49b534869becf0a94375cff8321c3c67d
okuzan <kuzanoleg@gmail.com>
2025-08-31 13:46:13 +0300

    removed avatar outline

--- src/app/shared/avatar/avatar.component.ts
+++ src/app/shared/avatar/avatar.component.ts
@@ -1,54 +1,30 @@
 import {Component, Input} from '@angular/core';
-import {TuiAvatar, TuiAvatarOutline, TuiSkeleton} from "@taiga-ui/kit";
+import {TuiAvatar, TuiSkeleton} from "@taiga-ui/kit";
 
 
 @Component({
   selector: 'app-avatar',
   template: `
-    <!--    ugly hack but I can't make tuiAvatarOutline work dynamically-->
-    @if (outline) {
-      <tui-avatar
-        [src]="avatarUrl || initials"
-        [size]="size"
-        [tuiSkeleton]="loading"
-        [style.background]="!avatarUrl ? 'var(--default-avatar-gradient)' : null"
-        [style.color]="'white'"
-        tuiAvatarOutline="var(--premium-gradient)"
-        [style.--t-size]="sizeInRem ? sizeInRem + 'rem' : null"
-        class="cursor-pointer"
-      >
-      </tui-avatar>
-    } @else {
-      <tui-avatar
-        [src]="avatarUrl || initials"
-        [style.background]="!avatarUrl ? 'var(--default-avatar-gradient)' : null"
-        [style.color]="'white'"
-        [size]="size"
-        [tuiSkeleton]="loading"
-        tuiAvatarOutline="var(--default-avatar-gradient)"
-        [style.--t-size]="sizeInRem ? sizeInRem + 'rem' : null"
-        class="cursor-pointer"
-      >
-      </tui-avatar>
-    }
+    <tui-avatar
+      [src]="avatarUrl || initials"
+      [size]="size"
+      [tuiSkeleton]="loading"
+      [style.background]="!avatarUrl ? 'var(--default-avatar-gradient)' : null"
+      [style.color]="'white'"
+      [style.--t-size]="sizeInRem ? sizeInRem + 'rem' : null"
+      class="cursor-pointer"
+    >
+    </tui-avatar>
   `,
-  styles: [`
-    tui-avatar {
-      ::ng-deep img {
-        padding: 4px;
-      }
-    }
-  `],
   imports: [
     TuiAvatar,
-    TuiAvatarOutline,
     TuiSkeleton
   ],
 })
 export class AvatarComponent {
   @Input() avatarUrl: string | null = null;
   @Input() username: string | null = null;
-  @Input() outline: boolean = false; // todo: rename to premium
+  @Input() outline: boolean = false; // todo: rename to premium. remove? Not used anymore.
   @Input() size: 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl' = 'm';
   @Input() sizeInRem: number | null = null;
   @Input() loading: boolean = false;
