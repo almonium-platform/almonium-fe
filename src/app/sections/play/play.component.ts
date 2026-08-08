1a7fc330f4c0ac04770fc4248b32af9570939d5a
okuzan <kuzanoleg@gmail.com>
2026-08-08 13:14:00 +0300

    apply native toggle to play filters

--- src/app/sections/play/play.component.ts
+++ src/app/sections/play/play.component.ts
@@ -1,12 +1,10 @@
 import {Component} from '@angular/core';
-import {NgClass} from "@angular/common";
 import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-play',
   templateUrl: './play.component.html',
   imports: [
-    NgClass,
     RouterLink,
   ],
   styleUrls: ['./play.component.less']
