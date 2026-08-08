afdd864ecf64af9d1d9ff59a9e39592f340701c0
okuzan <kuzanoleg@gmail.com>
2026-08-08 10:32:28 +0300

    fix payment success celebration trigger

--- src/app/static/payment-success/payment-success.component.ts
+++ src/app/static/payment-success/payment-success.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import {NgTemplateOutlet} from "@angular/common";
 import {UpgradeComponent} from "../../shared/upgrade/upgrade.component";
 import {UserInfoService} from "../../services/user-info.service";
+import {LocalStorageService} from "../../services/local-storage.service";
 
 @Component({
   selector: 'app-payment-success',
@@ -14,16 +15,17 @@ import {UserInfoService} from "../../services/user-info.service";
 })
 export class PaymentSuccessComponent implements OnInit {
   private userInfoService = inject(UserInfoService);
+  private localStorageService = inject(LocalStorageService);
   protected celebrate = false;
 
-  private readonly celebrationStorageKey = 'almonium.payment-success.celebrated';
+  private readonly celebrationStorageKey = 'payment_success_celebrated';
 
   ngOnInit() {
-    this.userInfoService.fetchUserInfoFromServer().subscribe(userInfo => {
-      if (userInfo?.premium && !sessionStorage.getItem(this.celebrationStorageKey)) {
-        sessionStorage.setItem(this.celebrationStorageKey, 'true');
-        this.celebrate = true;
-      }
-    });
+    if (!this.localStorageService.getItem<boolean>(this.celebrationStorageKey)) {
+      this.localStorageService.saveItem(this.celebrationStorageKey, true);
+      this.celebrate = true;
+    }
+
+    this.userInfoService.fetchUserInfoFromServer().subscribe();
   }
 }
